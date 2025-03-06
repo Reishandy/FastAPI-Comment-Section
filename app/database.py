c555d75cfb9055aa4b5acaa855aac8e10aa62796
Reishandy <isthisruxury@gmail.com>
2025-03-06 22:27:19 +0700

    authentication done

---
+++ app/database.py
@@ -0,0 +1,195 @@
+from datetime import datetime
+from re import match
+from os import getenv
+from urllib.parse import quote_plus
+from secrets import choice
+
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorClient, AsyncIOMotorCollection
+from pymongo.errors import OperationFailure
+from requests import post
+
+# Load the environment variables
+load_dotenv()
+
+# Global database variable
+DB: AsyncIOMotorDatabase
+
+
+# === DATABASE ===
+async def get_database() -> None:
+    """
+    Get the database connection using the environment variables.
+
+    variables:
+
+    - MONGODB_USERNAME: The username to connect to the MongoDB database.
+    - MONGODB_PASSWORD: The password to connect to the MongoDB database.
+    - MONGODB_DATABASE: The database name.
+    - MONGODB_HOST: The host of the MongoDB database.
+    - MONGODB_PORT: The port of the MongoDB database.
+    """
+    global DB
+
+    # Create connection string from environment variables
+    username = quote_plus(getenv("MONGODB_USERNAME"))
+    password = quote_plus(getenv("MONGODB_PASSWORD"))
+    database = getenv("MONGODB_DATABASE")
+    host = getenv("MONGODB_HOST")
+    port = int(getenv("MONGODB_PORT"))
+
+    mongodb_url = f"mongodb://{username}:{password}@{host}:{port}/{database}"
+
+    # Connect and return the database
+    client = AsyncIOMotorClient(mongodb_url)
+    DB = client[database]
+
+
+# === MAIN FLOW ===
+async def email_verification_queue(email: str, username: str = None) -> None:
+    """
+    Add the user into a registration or login queue, waiting for verification.
+
+    :param email: The email of the user.
+    :param username: The username of the user.
+    """
+    if not is_email_valid(email):
+        raise ValueError("Invalid email")
+
+    try:
+        # Generate a random numerical verification code
+        verification_code = generate_numerical_verification_code()
+
+        if username:
+            # From Register Endpoint
+            # Check if the user already exists
+            existing_user = await DB.users.find_one({"email": email})
+            if existing_user:
+                raise ValueError("User already exists")
+
+            # Replace the user if already exists
+            await DB.verification_queue.replace_one(
+                {"email": email},
+                {"email": email, "username": username, "verification_code": verification_code, "timestamp": datetime.now()},
+                upsert=True
+            )
+        else:
+            # From Login Endpoint
+            # Check if the user exists
+            existing_user = await DB.users.find_one({"email": email})
+            if not existing_user:
+                raise ValueError("404")
+
+            await DB.verification_queue.replace_one(
+                {"email": email},
+                {"email": email, "verification_code": verification_code, "timestamp": datetime.now()},
+                upsert=True
+            )
+
+        # Send the verification email
+        send_verification_email(email, verification_code)
+    except OperationFailure as e:
+        raise RuntimeError(str(e))
+
+
+async def verify_email(email: str, verification_code: str) -> str:
+    """
+    Verify the user using the email and verification code.
+
+    :param email: The email of the user.
+    :param verification_code: The verification code.
+    :return: An access token if the verification is successful.
+    """
+    try:
+        # Get the user from the verification queue
+        user = await DB.verification_queue.find_one({"email": email})
+        if not user:
+            raise ValueError("404")
+
+        stored_verification_code = user["verification_code"]
+        timestamp = user["timestamp"]
+
+        # Check if the verification code is correct
+        if verification_code != stored_verification_code:
+            raise ValueError("Invalid verification code")
+
+        # Check if the verification code is expired
+        if (datetime.now() - timestamp).total_seconds() > 600: # 10 minutes
+            raise ValueError("Verification code expired")
+
+        # Generate an access token
+        access_token = generate_access_token() + "_" + email
+
+        # Check if the user already registered
+        existing_user = await DB.users.find_one({"email": email})
+        if existing_user:
+            # From Login Queue
+            # If the user is already registered, add the access token to the user's access_token list
+            # Why using list? Because this will allow the user to log in from multiple devices.
+            await DB.users.update_one({"email": email}, {"$push": {"access_tokens": {"token": access_token, "timestamp": datetime.now()}}})
+        else:
+            # From Register Queue
+            # If the user is not registered, insert the user into the users collection
+            await DB.users.insert_one({"email": email, "username": user["username"], "access_tokens": [{"token": access_token, "timestamp": datetime.now()}]})
+
+        # Delete the user from the verification queue
+        await DB.verification_queue.delete_one({"email": email})
+
+        return access_token
+    except OperationFailure as e:
+        raise RuntimeError(str(e))
+
+
+# === HELPERS ===
+def is_email_valid(email: str) -> bool:
+    """
+    Check if the email is valid using a regular expression.
+
+    :param email: The email to check.
+    :return: True if the email is valid, False otherwise.
+    """
+    if bool(match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):
+        return True
+    return False
+
+
+def generate_numerical_verification_code(length: int = 6) -> str:
+    """
+    Generate a random numerical verification code.
+
+    :param length: The length of the verification code, default is 6.
+    :return: A random numerical verification code as a string.
+    """
+    return ''.join(choice("0123456789") for _ in range(length))
+
+
+def send_verification_email(email: str, verification_code: str) -> None:
+    """
+    Send a verification email using own custom email service.
+
+    :param email: The email to send the verification email to.
+    :param verification_code: The verification code to include in the email.
+    """
+    # INFO: This is an internal service, so the URL is hardcoded
+    url = "http://192.168.1.99:29998/email"
+    data = {
+        "recipient": email,
+        "subject": "Rei's Comment Section - Verify your email",
+        "plain": "Your Verification Code is: " + verification_code,
+        "html": "<table width=\"100%\" style=\"max-width: 600px; margin: auto; background: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0px 2px 5px rgba(0,0,0,0.1);\"> <tr> <td align=\"center\"> <h2 style=\"color: #007bff;\">Rei\'s Comment Section</h2> <p style=\"font-size: 16px; color: #555;\">Your verification code is:</p> <p style=\"font-size: 24px; font-weight: bold; color: #007bff; background: #f0f8ff; padding: 10px 20px; border-radius: 5px; display: inline-block;\"> " + verification_code + " </p> <p>This code will expire in 10 minutes.</p> <p style=\"color: #777;\">If you didn\'t request this code, please ignore this email.</p> </td> </tr> <tr> <td align=\"center\" style=\"padding-top: 20px; border-top: 1px solid #ddd;\"> <p style=\"font-size: 12px; color: #777;\"> Need help? Contact me at <a href=\"mailto:akbar@reishandy.my.id\" style=\"color: #007bff; text-decoration: none;\">akbar@reishandy.my.id</a> </p> <p style=\"font-size: 12px; color: #888;\"> <em>Legal Disclaimer:</em> This email may contain confidential information. If you are not the intended recipient, please delete it immediately. </p> </td> </tr></table>"
+    }
+
+    # Send the data using a requests POST request
+    response = post(url, json=data)
+
+    if response.status_code != 201:
+        raise RuntimeError("Failed to send verification email")
+
+
+def generate_access_token() -> str:
+    """
+    Generate a random access token.
+
+    :return: A random access token as a string.
+    """
+    return ''.join(choice("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(64))
---
+++ app/main.py
@@ -0,0 +1,195 @@
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+from fastapi import FastAPI, status, HTTPException, Body
+from pydantic import BaseModel, Field, EmailStr
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+import app.database as db_handler
+
+
+# === MODELS ===
+class User(BaseModel):
+    email: str = Field(..., description="The email of the user", examples=["john.doe@acme.com"])
+
+
+class Registration(User):
+    username: str = Field(..., description="The username of the user", examples=["user1", "user2"], min_length=3,
+                          max_length=20)
+
+
+class Verification(BaseModel):
+    email: str = Field(..., description="The email of the user", examples=["john.doe@acme.com"])
+    verification_code: str = Field(..., description="The verification code", examples=["123456"], min_length=6,
+                                   max_length=6)
+
+
+# === FASTAPI ===
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # INFO: Needs to set up environment variables before running the app, refer to README.md
+    # Get the database connection
+    await db_handler.get_database()
+
+    # TODO: Run an otp cleanup task every 24 hours
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+# Custom exception handler to change {detail} to {message}
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
+
+
+# === ENDPOINT ===
+@app.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Successful response",
+            "content": {"application/json": {"example": {"message": "ok"}}},
+        }})
+async def root() -> dict[str, str]:
+    """
+    Root endpoint, used to check if the service is running.
+    """
+    return {"message": "ok"}
+
+
+@app.post(
+    "/register",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_201_CREATED: {
+            "description": "Data created",
+            "content": {"application/json": {"example": {"message": "ok"}}},
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Bad request",
+            "content": {"application/json": {"example": {"message": "User already exists"}}},
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Internal server error",
+            "content": {"application/json": {"example": {"message": "Internal server error: <error message>"}}},
+        }})
+async def register(
+        registration_data: Annotated[Registration, Body(
+            title="User Registration details",
+            description="Endpoint to register a new user. Requires unused email, name, and password."
+        )]) -> dict[str, str]:
+    """
+    Register a new user with the email and username. The user will be placed in the verification queue.
+    Then go to /verify endpoint.
+
+    :param registration_data: User registration data
+    :return: {"message": "ok"} means the user has been placed in the verification queue
+    """
+    try:
+        await db_handler.email_verification_queue(**registration_data.model_dump())
+        return {"message": "ok"}
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Internal server error: {str(e)}")
+
+
+@app.post(
+    "/login",
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Successful response",
+            "content": {"application/json": {"example": {"message": "ok"}}},
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Bad request",
+            "content": {"application/json": {"example": {"message": "<error message>"}}},
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Not found",
+            "content": {"application/json": {"example": {"message": "User not found"}}},
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Internal server error",
+            "content": {"application/json": {"example": {"message": "Internal server error: <error message>"}}},
+        }})
+async def login(
+        user_data: Annotated[User, Body(
+            title="User Login details",
+            description="Endpoint to login a user. Requires email and password."
+        )]) -> dict[str, str]:
+    """
+    Login the user using the email. The user will be placed in the verification queue.
+    Then go to /verify endpoint.
+
+    :param user_data:  User login data
+    :return: {"message": "ok"} means the user has been placed in the verification queue
+    """
+    try:
+        await db_handler.email_verification_queue(**user_data.model_dump())
+        return {"message": "ok"}
+    except ValueError as e:
+        if str(e) == "404":
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Internal server error: {str(e)}")
+
+
+@app.post(
+    "/verify",
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Successful response",
+            "content": {"application/json": {"example": {"message": "ok", "access_token": "<access token>"}}},
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Bad request",
+            "content": {"application/json": {"example": {"message": "<error message>"}}},
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Not found",
+            "content": {"application/json": {"example": {"message": "User not found"}}},
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Internal server error",
+            "content": {"application/json": {"example": {"message": "Internal server error: <error message>"}}},
+        }})
+async def verify(
+        verification_data: Annotated[Verification, Body(
+            title="User Verification details",
+            description="Endpoint to verify the user. Requires email and verification code."
+        )]) -> dict[str, str]:
+    """
+    Verify the user using the email and verification code. If the verification is successful, return an access token.
+    This can be continued from register or login endpoint, and it will automatically detect and do the appropriate action.
+    Such as if from register endpoint, it will create a new user and return an access token. If from login endpoint, it will
+    return a new access token.
+
+    :return: {"message": "ok", "access_token": "<access token>"}
+    """
+
+    try:
+        access_token = await db_handler.verify_email(**verification_data.model_dump())
+        return {"message": "ok", "access_token": access_token}
+    except ValueError as e:
+        if str(e) == "404":
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        else:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Internal server error: {str(e)}")
